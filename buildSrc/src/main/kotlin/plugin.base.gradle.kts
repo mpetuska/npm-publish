04b7297902b31087da2177e222c1d4f7c433e782
Martynas Petuška <martynas@petuska.dev>
2022-05-03 14:58:05 +0100

    doc fixes

--- buildSrc/src/main/kotlin/plugin.base.gradle.kts
+++ buildSrc/src/main/kotlin/plugin.base.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
   id("io.gitlab.arturbosch.detekt")
   idea
@@ -23,4 +25,13 @@ repositories {
   mavenLocal()
   mavenCentral()
   gradlePluginPortal()
+}
+
+tasks.withType<Detekt>().configureEach {
+  reports {
+    html.required.set(true) // observe findings in your browser with structure and code snippets
+    xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
+    txt.required.set(true) // similar to the console output, contains issue signature to manually edit baseline files
+    sarif.required.set(true) // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with Github Code Scanning
+  }
 }
