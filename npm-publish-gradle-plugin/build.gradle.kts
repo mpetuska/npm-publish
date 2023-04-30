396e2c52d62d28f5efca612fb31953e1a1082fe6
Martynas Petuška <martynas@petuska.dev>
2023-04-30 09:58:39 +0100

    forgot to move signing config...

--- npm-publish-gradle-plugin/build.gradle.kts
+++ npm-publish-gradle-plugin/build.gradle.kts
@@ -79,3 +79,12 @@ publishing {
     }
   }
 }
+
+signing {
+  val signingKey: String? by project
+  val signingPassword: String? by project
+  if (signingKey != null) {
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications)
+  }
+}
